38fd8f3876a5dff0ae1601b241762c6b08147f06
lalit nailwal <lalit.nailwal2007@gmail.com>
2024-11-27 11:27:21 +0530

    main

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
